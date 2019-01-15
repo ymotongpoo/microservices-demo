2574a277509b2c85c838e4a44e16cb1ba105f66b
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2019-01-15 07:25:09 +0000

    productcatalog: change to use Stackdriver Logging

--- src/productcatalogservice/server.go
+++ src/productcatalogservice/server.go
@@ -31,10 +31,10 @@ import (
 	pb "github.com/GoogleCloudPlatform/microservices-demo/src/productcatalogservice/genproto"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 
+	"cloud.google.com/go/logging"
 	"cloud.google.com/go/profiler"
 	"contrib.go.opencensus.io/exporter/stackdriver"
 	"github.com/golang/protobuf/jsonpb"
-	"github.com/sirupsen/logrus"
 	"go.opencensus.io/plugin/ocgrpc"
 	"go.opencensus.io/stats/view"
 	"go.opencensus.io/trace"
@@ -43,10 +43,12 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+const projectID = "yoshifumi-cloud-demo/productcatalog"
+
 var (
 	cat          pb.ListProductsResponse
 	catalogMutex *sync.Mutex
-	log          *logrus.Logger
+	logger       *logging.Logger
 
 	port = flag.Int("port", 3550, "port to listen at")
 
@@ -54,20 +56,19 @@ var (
 )
 
 func init() {
-	log = logrus.New()
-	log.Formatter = &logrus.JSONFormatter{
-		FieldMap: logrus.FieldMap{
-			logrus.FieldKeyTime:  "timestamp",
-			logrus.FieldKeyLevel: "severity",
-			logrus.FieldKeyMsg:   "message",
-		},
-		TimestampFormat: time.RFC3339Nano,
+	ctx := context.Background()
+	client, err := logging.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Could not create Stackdriver Logging client: %v", err)
 	}
-	log.Out = os.Stdout
+	logger = client.Logger("productcatalog-logger")
 	catalogMutex = &sync.Mutex{}
 	err := readCatalogFile(&cat)
 	if err != nil {
-		log.Warnf("could not parse product catalog")
+		logger.Log(logging.Entry{
+			Severity: logging.Warning,
+			Payload:  "could not parse product catalog",
+		})
 	}
 }
 
@@ -81,18 +82,30 @@ func main() {
 	go func() {
 		for {
 			sig := <-sigs
-			log.Printf("Received signal: %s", sig)
+			logger.Log(logging.Entry{
+				Severity: logging.Info,
+				Payload:  fmt.Sprintf("Received signal: %s", sig),
+			})
 			if sig == syscall.SIGUSR1 {
 				reloadCatalog = true
-				log.Infof("Enable catalog reloading")
+				logger.Log(logging.Entry{
+					Severity: logging.Info,
+					Payload:  "Enable catalog reloading",
+				})
 			} else {
 				reloadCatalog = false
-				log.Infof("Disable catalog reloading")
+				logger.Log(logging.Entry{
+					Severity: logging.Info,
+					Payload:  "Disable catalog reloading",
+				})
 			}
 		}
 	}()
 
-	log.Infof("starting grpc server at :%d", *port)
+	logger.Log(logging.Entry{
+		Severity: logging.Info,
+		Payload:  fmt.Sprintf("starting grpc server at :%d", *port),
+	})
 	run(*port)
 	select {}
 }
@@ -100,7 +113,10 @@ func main() {
 func run(port int) string {
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		log.Fatal(err)
+		logger.Log(logging.Entry{
+			Severity: logging.Critical,
+			Payload:  err.Error(),
+		})
 	}
 	srv := grpc.NewServer(grpc.StatsHandler(&ocgrpc.ServerHandler{}))
 	svc := &productCatalog{}
@@ -114,9 +130,15 @@ func initStats(exporter *stackdriver.Exporter) {
 	view.SetReportingPeriod(60 * time.Second)
 	view.RegisterExporter(exporter)
 	if err := view.Register(ocgrpc.DefaultServerViews...); err != nil {
-		log.Info("Error registering default server views")
+		logger.Log(logging.Entry{
+			Severity: logging.Info,
+			Payload:  "Error registering default server views",
+		})
 	} else {
-		log.Info("Registered default server views")
+		logger.Log(logging.Entry{
+			Severity: logging.Info,
+			Payload:  "Registered default server views",
+		})
 	}
 }
 
@@ -126,21 +148,33 @@ func initTracing() {
 	for i := 1; i <= 3; i++ {
 		exporter, err := stackdriver.NewExporter(stackdriver.Options{})
 		if err != nil {
-			log.Warnf("failed to initialize stackdriver exporter: %+v", err)
+			logger.Log(logging.Entry{
+				Severity: logging.Warning,
+				Payload:  fmt.Sprintf("failed to initialize stackdriver exporter: %+v", err),
+			})
 		} else {
 			trace.RegisterExporter(exporter)
 			trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
-			log.Info("registered stackdriver tracing")
+			logger.Log(logging.Entry{
+				Severity: logging.Info,
+				Payload:  "registered stackdriver tracing",
+			})
 
 			// Register the views to collect server stats.
 			initStats(exporter)
 			return
 		}
 		d := time.Second * 10 * time.Duration(i)
-		log.Infof("sleeping %v to retry initializing stackdriver exporter", d)
+		logger.Log(logging.Entry{
+			Severity: logging.Info,
+			Payload:  fmt.Printf("sleeping %v to retry initializing stackdriver exporter", d),
+		})
 		time.Sleep(d)
 	}
-	log.Warn("could not initialize stackdriver exporter after retrying, giving up")
+	logger.Log(logging.Entry{
+		Severity: logging.Warning,
+		Payload:  "could not initialize stackdriver exporter after retrying, giving up",
+	})
 }
 
 func initProfiling(service, version string) {
@@ -153,16 +187,27 @@ func initProfiling(service, version string) {
 			// ProjectID must be set if not running on GCP.
 			// ProjectID: "my-project",
 		}); err != nil {
-			log.Warnf("failed to start profiler: %+v", err)
+			logger.Log(logging.Entry{
+				Severity: logging.Warning,
+				Payload:  fmt.Sprintf("failed to start profiler: %+v", err),
+			})
 		} else {
-			log.Info("started stackdriver profiler")
+			logger.Log(logging.Entry{
+				Severity: logging.Info,
+				Payload:  "started stackdriver profiler",
+			})
 			return
 		}
 		d := time.Second * 10 * time.Duration(i)
-		log.Infof("sleeping %v to retry initializing stackdriver profiler", d)
-		time.Sleep(d)
+		logger.Log(logging.Entry{
+			Severity: logging.Info,
+			Payload:  fmt.Printf("sleeping %v to retry initializing stackdriver profiler", d),
+		})
 	}
-	log.Warn("could not initialize stackdriver profiler after retrying, giving up")
+	logger.Log(logging.Entry{
+		Severity: logging.Warning,
+		Payload:  "could not initialize stackdriver profiler after retrying, giving up",
+	})
 }
 
 type productCatalog struct{}
@@ -172,14 +217,23 @@ func readCatalogFile(catalog *pb.ListProductsResponse) error {
 	defer catalogMutex.Unlock()
 	catalogJSON, err := ioutil.ReadFile("products.json")
 	if err != nil {
-		log.Fatalf("failed to open product catalog json file: %v", err)
+		logger.Log(logging.Entry{
+			Severity: logging.Critical,
+			Payload:  fmt.Printf("failed to open product catalog json file: %v", err),
+		})
 		return err
 	}
 	if err := jsonpb.Unmarshal(bytes.NewReader(catalogJSON), catalog); err != nil {
-		log.Warnf("failed to parse the catalog JSON: %v", err)
+		logger.Log(logging.Entry{
+			Severity: logging.Warning,
+			Payload:  fmt.Printf("failed to parse the catalog JSON: %v", err),
+		})
 		return err
 	}
-	log.Info("successfully parsed product catalog json")
+	logger.Log(logging.Entry{
+		Severity: logging.Info,
+		Payload:  "successfully parsed product catalog json",
+	})
 	return nil
 }
 
